58cc52ab1f4a59e1aa5b17712ae4d3b81a24611d
Saif-Eldeen-95 <es.seifeldin.shady2023@gmail.com>
2026-04-09 14:27:18 +0200

    implementing the first half of requirements

--- RBTree.py
+++ RBTree.py
@@ -1,3 +1,4 @@
+# creation **************************************************************************
 def create_node(key):
     return {
         "key": key,
@@ -14,6 +15,7 @@ def create_tree():
         "NIL": NIL
     }
 
+# insertion and fixing **************************************************************************
 def left_rotate(tree, x):
     NIL = tree["NIL"]
     y = x["right"]
@@ -53,34 +55,6 @@ def right_rotate(tree, x):
 
     y["right"] = x
     x["parent"] = y
-
-def insert(tree, key):
-    NIL = tree["NIL"]
-    node = create_node(key)
-    node["left"] = NIL
-    node["right"] = NIL
-
-    parent = None
-    current = tree["root"]
-
-    while current != NIL:
-        parent = current
-        if node["key"] < current["key"]:
-            current = current["left"]
-        else:
-            current = current["right"]
-
-    node["parent"] = parent
-
-    if parent is None:
-        tree["root"] = node
-    elif node["key"] < parent["key"]:
-        parent["left"] = node
-    else:
-        parent["right"] = node
-
-    node["color"] = "RED"
-    fix_insert(tree, node)
 
 def fix_insert(tree, k):
     NIL = tree["NIL"]
@@ -123,3 +97,89 @@ def fix_insert(tree, k):
                 left_rotate(tree, k["parent"]["parent"])
 
     tree["root"]["color"] = "BLACK"
+
+def insert(tree, key):
+    NIL = tree["NIL"]
+    node = create_node(key)
+    node["left"] = NIL
+    node["right"] = NIL
+
+    parent = None
+    current = tree["root"]
+
+    while current != NIL:
+        parent = current
+        if node["key"] < current["key"]:
+            current = current["left"]
+        else:
+            current = current["right"]
+
+    node["parent"] = parent
+
+    if parent is None:
+        tree["root"] = node
+    elif node["key"] < parent["key"]:
+        parent["left"] = node
+    else:
+        parent["right"] = node
+
+    node["color"] = "RED"
+    fix_insert(tree, node)
+
+# array to tree **************************************************************************
+def array_to_tree(arr):
+    tree = create_tree()
+    for key in arr:
+        insert(tree, key)
+    return tree
+
+# print tree **************************************************************************     
+def print_tree(tree):
+    def inorder(node):
+        if node != tree["NIL"]:
+            inorder(node["left"])
+            print(node["key"], end=" ")
+            inorder(node["right"])
+    inorder(tree["root"])
+
+# search **************************************************************************     
+def search(tree, key):
+    NIL = tree["NIL"]
+    current = tree["root"]
+    while current != NIL and key != current["key"]:
+        if key < current["key"]:
+            current = current["left"]
+        else:
+            current = current["right"]
+    return current
+
+#print tree height **************************************************************************     
+def print_tree_height(tree):
+    def height(node):
+        if node == tree["NIL"]:
+            return 0
+        return 1 + max(height(node["left"]), height(node["right"]))
+    return height(tree["root"])
+
+#print tree black height **************************************************************************     
+def print_tree_black_height(tree):
+    def black_height(node):
+        if node == tree["NIL"]:
+            return 0
+        return 1 + black_height(node["left"])
+    return black_height(tree["root"])
+
+#print tree size **************************************************************************     
+def print_tree_size(tree):
+    def size(node):
+        if node == tree["NIL"]:
+            return 0
+        return 1 + size(node["left"]) + size(node["right"])
+    return size(tree["root"])
+
+# test
+arr = [10, 70, 30, 40, 20, 60, 50]
+tree = array_to_tree(arr)
+print_tree(tree)
+print(print_tree_height(tree))
+print(print_tree_black_height(tree))
